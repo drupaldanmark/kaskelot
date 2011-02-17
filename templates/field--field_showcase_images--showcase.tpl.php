6ea132bf53d0d122cb41c7a780546ae4dc03e817
Mikkel Hoegh <mikkel@hoegh.org>
2011-02-17 22:49:54 +0100

    Render only the first image in showcase views.

---
+++ templates/field--field_showcase_images--showcase.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Template for rendering showcase images in the showcase view.
+ */
+
+// We only want to render the first image in these views.
+print render($items[0]);
+
